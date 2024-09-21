eb6757e6b7dc250c924f7604c320401394a7e214
usman4222 <usman4243ch@gmail.com>
2024-09-21 21:43:19 +0500

    Complete api of add and update image

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,4 +1,5 @@
 import User from "../models/AuthModel.js";
+import { renameSync, unlinkSync } from "fs"
 
 export const getUserData = async (req, res) => {
 
@@ -56,4 +57,61 @@ export const updateUserProfile = async (req, res) => {
         console.log(error);
         return res.status(500).send("Internel server error")
     }
+}
+
+
+export const addProfileImage = async (req, res) => {
+
+    if (!req.file) {
+        return res.status(400).send("Image is required");
+    }
+
+    const date = Date.now();
+    const fileName = `uploads/profiles/${date}${req.file.originalname}`;
+
+    renameSync(req.file.path, fileName);
+
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+            req.userId,
+            { image: fileName },
+            { new: true, runValidators: true }
+        );
+
+        if (!updatedUser) {
+            return res.status(404).send("User not found");
+        }
+
+        return res.status(200).json({
+            image: updatedUser.image
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send("Server error");
+    }
+};
+
+
+export const delProfileImage = async (req, res) => {
+    try {
+        const { userId } = req
+        const user = await User.findById(userId)
+
+        if (!user) {
+            return res.status(400).send("User not exist.")
+        }
+
+        if (user.image) {
+            unlinkSync(user.image)
+        }
+        user.image = null
+        await user.save()
+
+        return res.status(200).json("Profile Image deleted successfully.")
+        
+
+    } catch (error) {
+    console.log(error);
+    return res.status(500).send("Internel server error")
+}
 }
