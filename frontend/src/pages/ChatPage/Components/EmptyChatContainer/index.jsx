d170d76851c6d0ce4c45b56c952badf70c281c4b
Rana Usman <usman4243ch@gmail.com>
2024-09-24 15:11:07 -0700

    Add Empty Chat Page

---
+++ frontend/src/pages/ChatPage/Components/EmptyChatContainer/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Lottie from "react-lottie";
+import { animationDefaultOption } from "../../../../lib/untils";
+
+const EmptyChatContainer = () => {
+  return (
+    <div className="flex-1 md:bg-[#1c1d25] md:flex flex-col justify-center items-center duration-1000 transition-all">
+      <Lottie
+        isClickToPauseDisabled={true}
+        height={300}
+        width={500}
+        options={{ ...animationDefaultOption }}
+      />
+      <div className="text-opacity-80 flex flex-col gap-5 items-center mt-10 lg:text-4xl text-3xl transition-all duration-300 text-encter">
+        <h3 className="poppins-medium">
+          Hi <span className="text-purple-500">! </span>Welcome
+          <span className="text-purple-500"> to Chat App</span>
+        </h3>
+      </div>
+    </div>
+  );
+};
+
+export default EmptyChatContainer;
