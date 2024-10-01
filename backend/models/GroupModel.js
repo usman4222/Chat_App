507a36e2aa823949e1d1818d75423ad086e2137d
usman4222 <usman4243ch@gmail.com>
2024-10-01 17:14:18 +0500

    Create Groups Model

---
+++ backend/models/GroupModel.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const groupSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  members: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: "Users",
+      required: true,
+    },
+  ],
+  admin: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Users",
+    required: true,
+  },
+  messages: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: "Messages",
+      required: fasle,
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  updated: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+groupSchema.pre("save", function (next) {
+  this.updatedAt = Date.now();
+  next();
+});
+
+groupSchema.pre("findOneAndUpdate", function (next) {
+  this.set({ updatedAt: Date.now() });
+  next();
+});
+
+const Group = mongoose.model("Groups", groupSchema);
+
+export default Group;
