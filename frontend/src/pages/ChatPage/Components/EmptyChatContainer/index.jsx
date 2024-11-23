bc3d3fe9ff53fd180de604e98a3bfd0d1dae261d
usman4222 <usman4243ch@gmail.com>
2024-11-23 08:10:26 -0800

    Update empty chat container

--- frontend/src/pages/ChatPage/Components/EmptyChatContainer/index.jsx
+++ frontend/src/pages/ChatPage/Components/EmptyChatContainer/index.jsx
@@ -14,7 +14,7 @@ const EmptyChatContainer = () => {
       <div className="text-opacity-80 flex flex-col gap-5 items-center mt-10 lg:text-4xl text-3xl transition-all duration-300 text-encter">
         <h3 className="poppins-medium">
           Hi <span className="text-purple-500">! </span>Welcome
-          <span className="text-purple-500"> to Chat App</span>
+          <span className="text-purple-500"> to ChatApp</span>
         </h3>
       </div>
     </div>
